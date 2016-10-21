9912af20df342a5cd4637cebf0532f91888d922b
Pranay Patil <pranay.patil.dev@gmail.com>
2016-10-21 20:37:09 +0530

    initial Commit

---
+++ Clustering/k_means.py
@@ -0,0 +1,26 @@
+__author__ = 'Pranay'
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+from sklearn.cluster import KMeans
+
+x = np.array([[1, 2],
+              [1.5, 1.8],
+              [5, 8],
+              [8, 8],
+              [1, 0.6],
+              [9, 11]])
+
+clf = KMeans(n_clusters=2)
+clf.fit(x)
+
+centroids = clf.cluster_centers_
+labels = clf.labels_
+
+colors = ['r.', 'g.', 'c.', 'b.', 'k.']
+
+for i in range(len(x)):
+    plt.plot(x[i][0], x[i][1], colors[labels[i]], markersize=10)
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=150)
+plt.show()
